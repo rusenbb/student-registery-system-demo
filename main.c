de00a1a58622976dd3956156d7be9cfb0d09558d
rusen14 <70446242+rusen14@users.noreply.github.com>
2022-04-19 14:16:25 +0300

    modified displaying functions. created a main display function. revised comments. added a sorting algorithm (not finished yet).

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_menu();
 struct Node *add_record();
 int dis_by_id();
 int dis_by_year();
 int dis_by_fac();
 int del_rec();
 int sort();
+void print_menu();
+void display_stack();
 
 struct Node
 {
@@ -17,13 +18,13 @@ struct Node
     struct Node *next;
 };
 
-// our record stack
-struct Node *record_stack[200];
-int stack_marker = -1; // index of the last record
+// our record stacks
+struct Node *record_stack[500]; // main stack (is used when a record is added)
+int stack_marker = 0;           // number of records on the main stack
 
 int main()
 {
-    while (1)
+    while (1) // infinite loop
     {
         // Getting option from the user
         int choice;
@@ -32,10 +33,11 @@ int main()
 
         if (choice == 1)
         {
-            stack_marker++;
-            struct Node *record;
+            // add record, increment record count
+            struct Node *record = (struct Node *)malloc(sizeof(struct Node *));
             record = add_record();
             record_stack[stack_marker] = record;
+            stack_marker++;
         }
         else if (choice == 2)
         {
@@ -43,23 +45,26 @@ int main()
         }
         else if (choice == 3)
         {
-            // dis_by_year();
+            dis_by_year();
         }
         else if (choice == 4)
         {
-            // dis_by_fac();
+            dis_by_fac();
         }
         else if (choice == 5)
         {
+            // del record, decrement count
+            stack_marker--;
             // del_rec();
         }
         else if (choice == 6)
         {
+            // exit the program
             exit(0);
         }
         else
         {
-            printf("Wrong option. Please pick a valid one.");
+            printf("Wrong option. Please pick a valid one.\n");
         }
     }
 }
@@ -69,68 +74,103 @@ struct Node *add_record()
     /*Adds the pointer of records to the main stack.
     Records are in the form of a linked list:
     student_id->n->a->m->e */
-    struct Node *head;
+    struct Node *head = (struct Node *)malloc(sizeof(struct Node *));
     struct Node *current_pt = NULL;
     struct Node *node = (struct Node *)malloc(sizeof(struct Node *));
     head = node;
     current_pt = node;
 
     printf("\nEnter school number: ");
-    scanf("%9d\n", &(node->data));
+    scanf("%9d", &(node->data));
+    getchar();
+    printf("\nEnter  Name Surname: ");
     int c;
     while ((c = getchar()) != '\n' && c != EOF)
     {
-        struct Node *new_Node = (struct Node *)malloc(sizeof(struct Node *));
+        struct Node *new_Node = (struct Node *)malloc(sizeof(struct Node));
         current_pt->next = new_Node;
         current_pt = new_Node;
         current_pt->next = NULL;
         current_pt->data = c;
     }
+    current_pt->next = NULL;
     return head;
 }
 
 int dis_by_id()
 {
-    sort();
+    // sort();
+    display_stack();
+}
 
+int dis_by_year()
+{
+    // sort();
+    display_stack();
+}
+
+int dis_by_fac()
+{
+    sort(9, 7);
+    display_stack();
+}
+
+int del_rec()
+{
+    // sort();
+}
+
+int sort(int start_id, int end_id)
+{
+    /*
+    does sorting given which parts of student id is considered when sorting
+    insertion sort is used
+    start & end id: which part of student id is used when sorting
+    e.g. -> start_id = 9, ed_id = 7, heads[0]->data = 123456789
+    123 is cropped and used for sorting
+    */
+
+    for (int i = 1; i < stack_marker; i++)
+    {
+        int value_to_insert;
+        int compared_value;
+        int j;
+        struct Node *value_to_insert_pt;
+
+        j = i;
+
+        value_to_insert = record_stack[i]->data; // BOOKMARK: THIS WILL BE MODIFIED
+        value_to_insert_pt = record_stack[i];
+
+        while (j > 0 && (compared_value = record_stack[j - 1]->data) > value_to_insert) // AND THIS TOO
+        {
+            record_stack[j] = record_stack[j - 1];
+            j--;
+        }
+        record_stack[j] = value_to_insert_pt;
+    }
+}
+
+void display_stack()
+{
     struct Node *record_head;
 
-    for (int i = 0; i <= stack_marker; i++) // for each record till stack_marker
+    for (int i = 0; i < stack_marker; i++) // for each record till stack_marker
     {
         printf("School ID: ");
         record_head = record_stack[i];
         printf("%d", record_head->data);
         record_head = record_head->next;
 
-        printf("\nName - Surname: ");
-        while (record_head->next != NULL)
+        printf("\n");
+        printf("Name Surname: ");
+        while (record_head != NULL)
         {
             printf("%c", record_head->data);
             record_head = record_head->next;
         }
-        printf("%c\n\n", record_head->data); // last character has null ->next, so print explicitly
         printf("\n");
     }
-}
-
-int dis_by_year()
-{
-    sort();
-}
-
-int dis_by_fac()
-{
-    sort();
-}
-
-int del_rec()
-{
-    sort();
-}
-
-int sort()
-{
-    // does sorting given ...
 }
 
 void print_menu()
